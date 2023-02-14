d268d476834ca77634b46265096de9964cd1023f
Anthony Simental <anthonysimental12@gmail.com>
2023-02-13 23:39:17 -0800

    added first Event Listener ('DOMContentLoaded')

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 // Global Variables
 const baseURL = 'https://api.openbrewerydb.org/breweries';
+
+
+// Startup (Event Listener)
+window.addEventListener('DOMContentLoaded', () => {
+    getBreweries();
+})
