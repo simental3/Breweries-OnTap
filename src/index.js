198895ca3bde1261a2ee99864449a8027b2bac88
Anthony Simental <anthonysimental12@gmail.com>
2023-02-14 16:27:14 -0800

    added a light/dark mode using an event listener

--- src/index.js
+++ src/index.js
@@ -63,3 +63,12 @@ const renderBrewery = (event) => {
         <p>${data.website_url}</p>`
     })
 }
+
+
+const sunMoonContainer = document.querySelector('.sun-moon-container');
+
+document.querySelector('.theme-toggle-button').addEventListener('click', () => {
+    document.body.classList.toggle('dark')
+    const currentRotation = parseInt(getComputedStyle(sunMoonContainer).getPropertyValue('--rotation'))
+    sunMoonContainer.style.setProperty('--rotation', currentRotation + 180)
+})
